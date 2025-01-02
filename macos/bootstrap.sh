5efad2d093f4bc5052349f6f03c30db90ab64953
Rinat Zaripov <rinat@propelleraero.com.au>
2025-01-02 15:57:14 +1100

    chore: add homebrew to the path

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -4,6 +4,7 @@
 # if homebrew is not installed, install it
 if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'export PATH="/opt/homebrew/bin:$PATH"' >> ~/.zshrc
 fi
 
 if [ ! -d ~/Projects/flakes ]; then
