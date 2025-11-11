328f0c6f392573bc6d21411343d62681aff0a82a
Rinat Zaripov <zaripov.rinat@gmail.com>
2025-11-11 20:31:17 +1100

    chore: test separating out macos flake

--- features/television/module.nix
+++ features/television/module.nix
@@ -14,7 +14,7 @@
 
       package = let
         nixpkgs =
-          if inputs.nixpkgs.sourceInfo == inputs.nixpkgs-unstable.sourceInfo
+          if !inputs ? nixpkgs-unstable || inputs.nixpkgs.sourceInfo == inputs.nixpkgs-unstable.sourceInfo
           then pkgs
           else (import inputs.nixpkgs-unstable {system = pkgs.system;});
       in
---
+++ macos/flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "A macOS-only configuration/module flake for my personal laptops";
+
+  inputs = {
+    systems.url = "github:nix-systems/aarch64-darwin";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-compat.url = "github:hraban/flake-compat";
+    flake-compat.flake = false;
+
+    mac-app-utils.url = "github:hraban/mac-app-util";
+    mac-app-utils.inputs.flake-utils.follows = "flake-utils";
+    mac-app-utils.inputs.systems.follows = "systems";
+    mac-app-utils.inputs.flake-compat.follows = "flake-compat";
+
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+
+    nvf.url = "github:notashelf/nvf/v0.8";
+    nvf.inputs.systems.follows = "systems";
+    nvf.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.inputs.flake-parts.follows = "flake-parts";
+    nvf.inputs.flake-compat.follows = "flake-compat";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    nix-darwin,
+    nixpkgs-lib,
+    ...
+  }: let
+    lib = import ../libs/default.nix {
+      inherit inputs;
+    };
+  in
+    lib.mkFlake {
+      inherit inputs;
+    } {
+      systems = [
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        ../options/libs/part.nix
+        ../options/nix-darwin/part.nix
+
+        {
+          flake.lib = lib;
+        }
+
+        ../features/nvim/part.nix
+        ../features/zsh/part.nix
+
+        {
+          flake.darwinModules.default = ./module.nix;
+
+          flake.darwinConfigurations.default = lib.mkHostConfig {
+            system = "aarch64-darwin";
+            modules = [
+              ./module.nix
+            ];
+          };
+        }
+      ];
+    };
+}
