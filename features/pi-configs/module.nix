eb0dc89448ad70148449dfce410be2ae0054047c
Rinat Zaripov <zaripov.rinat@gmail.com>
2026-05-29 19:56:39 +1000

    fix(pi-configs): suppress undici warning

--- features/pi-configs/module.nix
+++ features/pi-configs/module.nix
@@ -5,12 +5,22 @@
 }: let
   claudeTmpDir = "/Users/${username}/.cache/pi/tmp";
   piAgentDir = "/Users/${username}/.pi/agent";
+  suppressUndiciExperimentalProxyAgentWarning = ''
+    case " ''${NODE_OPTIONS-} " in
+      *" --disable-warning=UNDICI-EHPA "*) ;;
+      *) export NODE_OPTIONS="''${NODE_OPTIONS:+$NODE_OPTIONS }--disable-warning=UNDICI-EHPA" ;;
+    esac
+  '';
 in {
   imports = [../home-manager/module.nix];
 
   environment.variables = {
     CLAUDE_TMPDIR = claudeTmpDir;
   };
+
+  # Append to NODE_OPTIONS instead of setting it as a plain env var so existing
+  # options are preserved and the warning suppression is not duplicated.
+  environment.extraInit = suppressUndiciExperimentalProxyAgentWarning;
 
   launchd.user.envVariables = {
     CLAUDE_TMPDIR = claudeTmpDir;
@@ -20,6 +30,7 @@ in {
     home.sessionVariables = {
       CLAUDE_TMPDIR = claudeTmpDir;
     };
+    home.sessionVariablesExtra = suppressUndiciExperimentalProxyAgentWarning;
 
     xdg.cacheFile."pi/tmp/.keep".text = "";
   };
@@ -40,6 +51,10 @@ in {
     "${piAgentDir}/settings.json" = {
       flake = inputs.self;
       linkFrom = "features/pi-configs/configs/settings.json";
+    };
+    "${piAgentDir}/web-fetch.json" = {
+      flake = inputs.self;
+      linkFrom = "features/pi-configs/configs/web-fetch.json";
     };
   };
 }
